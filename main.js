91f155f6231057fe44545c7d701f492511acd735
X-SUMUKH-X <97395793+X-SUMUKH-X@users.noreply.github.com>
2026-06-20 11:05:45 +0530

    Install and initialize Vercel Analytics

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+import { inject } from '@vercel/analytics';
+
+// Initialize Vercel Analytics
+inject();
+
 // Theme Toggle Control
 function setTheme(theme) {
     const html = document.documentElement;
